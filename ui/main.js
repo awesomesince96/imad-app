3c17eef54059b9b4343705ff54c3f6afd93b22b4
awesomesince96 <krishna7902@gmail.com>
2017-09-22 23:14:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onClick = function(){
+    
+    var request = new XMLHttpRequest();
+     
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status ===200 ){
+                var counter = request.responseText;
+                
+                var count = document.getElementById('count');
+                count.innerHTML = counter.toString();
+            }
+            
+        }
+    }; 
+    
+}
