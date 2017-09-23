00f7f2a09df42ccdfcf3455f8b3e6a96441091a4
awesomesince96 <krishna7902@gmail.com>
2017-09-23 08:28:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,18 +2,19 @@ var button = document.getElementById('counter');
 
 button.onClick = function(){
     
+    // Make a request to counter endpoint
     var request = new XMLHttpRequest();
      
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            
-            if(request.status ===200 ){
-                var counter = request.responseText;
-                
+    // Capture the response and store it in a variable 
+    request.onreadystatechange = function(){                //onreadystatechange detects the change in request like sent,sending etc.
+        if(request.readyState === XMLHttpRequest.DONE){     // Now check the request state if it is completly done
+                                                            // Take some actions
+            if(request.status === 200 ){                    // Now check if the request made was an successfull one
+                var counter = request.responseText;         // responseText gives access to response value
                 var count = document.getElementById('count');
                 count.innerHTML = counter.toString();
             }
-            
+                                                            // if not done yet do here
         }
     }; 
     
